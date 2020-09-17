4df7eef240069bf1d6afde82f2b460b1f48ef178
kevinShikuku <kevinshikuku255@gmail.com>
2020-09-17 11:11:19 +0300

    WEBSOCKET_API_URL

--- src/Utils/Apolloprovider.js
+++ src/Utils/Apolloprovider.js
@@ -12,15 +12,16 @@ const API_URL = process.env.REACT_APP_API_URL;
 // GraphQL WebSocket (subscriptions) URL.
 // If its url is not set in .env then it has same url, host and pathname
 const WEBSOCKET_API_URL = process.env.REACT_APP_WEBSOCKET_API_URL;
-const websocketApiUrl = WEBSOCKET_API_URL
-  ? WEBSOCKET_API_URL
-    : API_URL.replace('https://', 'wss://').replace('https://', 'wss://');
+
+// const websocketApiUrl = WEBSOCKET_API_URL
+//   ? WEBSOCKET_API_URL
+//     : API_URL.replace('https://', 'wss://').replace('https://', 'wss://');
 
 
 
 
 // Create a Apollo client
-const apolloClient = createApolloClient(API_URL, websocketApiUrl);
+const apolloClient = createApolloClient(API_URL, WEBSOCKET_API_URL);
 
 
 
