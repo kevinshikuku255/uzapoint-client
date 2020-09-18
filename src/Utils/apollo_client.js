b57b7e41ca5aec47d9f1895b976616f0759436b2
kevinShikuku <kevinshikuku255@gmail.com>
2020-09-18 18:34:42 +0300

    json

--- src/Utils/apollo_client.js
+++ src/Utils/apollo_client.js
@@ -29,6 +29,7 @@ const createAuthLink = () =>  setContext((_, { headers }) => {
   return {
     headers: {
       Authorization: token ? token : "",
+      accept: 'application/json',
     }
   }
 });
