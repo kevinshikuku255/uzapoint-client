2e89c89c3292de68824e9cf2e1b3356e61e8852f
kevinShikuku <kevinshikuku255@gmail.com>
2020-10-04 13:37:59 +0300

    sunday2

--- public/sw.js
+++ public/sw.js
@@ -1,88 +1,34 @@
-const expectedCaches = [
-   'static-v1'
-]
+const expectedCaches = [ 'static-v1' ]
 
 const urlsToCache = ["/appShell.html"];
 
 const self = this;
-//events
+
+
 //1. Instalation of SW
   self.addEventListener('install', (event) => {
-   //   self.skipWaiting();
+     self.skipWaiting();
       event.waitUntil(
          caches.open("static-v1")
-          .then( cache => cache.addAll(urlsToCache))
+          .then( cache => cache.addAll(urlsToCache)
+              .then(() => console.log("caches are ready..."))
+           )
       )
   })
 
 
 
 // 2. Listen for requests
+
  self.addEventListener('fetch', (event) => {
  event.respondWith(
     caches.match(event.request)
-     .then(response => response || fetch(event.request))
+     .then(response => response || caches.match("/appShell.html"))
      .catch( () => {
             return caches.match("/appShell.html")
      })
  )
  });
-
-
-
-
-// self.addEventListener('fetch', function(event) {
-
-//     event.respondWith(async function() {
-//         const cache = await caches.open('cache-v1');
-//         console.log(cahce)
-//         const cachedResponse = await cache.match(event.request);
-//         const fetchPromise = fetch(event.request);
-
-//         const networkResponse = await fetchPromise;
-//         console.log(networkResponse)
-//         event.waitUntil(async function () {
-
-//             // Update the cache with a newer version
-//             await cache.put(event.request, networkResponse.clone());
-//         }());
-
-//         // The response contains cached data, if available
-//         return cachedResponse || networkResponse
-//     }());
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
