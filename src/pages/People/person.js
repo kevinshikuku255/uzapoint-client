b9499512f8936ae298bb7b469978768dbafd7037
kevinshikuku255 <68957078+kevinshikuku255@users.noreply.github.com>
2021-05-02 00:33:15 +0300

    Update person.js

--- src/pages/People/person.js
+++ src/pages/People/person.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {useHistory} from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
 import "./people.css";
-import {Image, Transformation, Placeholder} from 'cloudinary-react';
+
 
 
 function Person({person}) {
- const {username, imagePublicId, phonenumber} = person;
+ const {username, image, phonenumber} = person;
  const history = useHistory();
 
  const toProfile = () =>{
@@ -17,13 +17,7 @@ function Person({person}) {
  return (
   <div className="person_wrapper" onClick={toProfile} >
       <div className="person_header">
-         {imagePublicId ? <Image
-            publicId={imagePublicId}
-            loading="lazy"
-            >
-            <Transformation height="20rem" width="20rem" crop="fill" radius="50%"/>
-            <Placeholder type="blur"/>
-          </Image>: <Avatar alt="avator"/>}
+         {image ? <Avatar alt={username} src={image} /> : <Avatar alt="avator"/>}
          <div className="person_contact">
             <h4>{username}</h4>
             <p>{phonenumber}</p>
