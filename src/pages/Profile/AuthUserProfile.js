3f3fb28e4ec71a654a146823c4af34aeda979404
kevinshikuku255 <68957078+kevinshikuku255@users.noreply.github.com>
2021-05-01 23:39:15 +0300

    Update AuthUserProfile.js

--- src/pages/Profile/AuthUserProfile.js
+++ src/pages/Profile/AuthUserProfile.js
@@ -139,7 +139,7 @@ const main =
               <LazyLoadImage
                 alt={username}
                 effect="blur"
-                height="15rem"
+                height="30rem"
                 width="100%"
                 loading="lazy"
                 src={coverImage}/>
